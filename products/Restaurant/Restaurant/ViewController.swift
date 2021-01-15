f9f9917a99e0bb7e1253afb94752d677b3ed97ba
cookie777 <l.oeil.ballon@gmail.com>
2021-01-14 23:16:38 -0800

    add: restrants app

---
+++ products/Restaurant/Restaurant/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Restaurant
+//
+//  Created by Takayuki Yamaguchi on 2021-01-14.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    
+    view.backgroundColor = .cyan
+  }
+
+
+}
+
